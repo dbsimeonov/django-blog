29f22590813cc7d5142c521c19e00520bed81d76
Daniel <dbsimeonov@yahoo.com>
2019-05-15 13:51:04 +0100

    Created Articles.app

--- djangonautic/urls.py
+++ djangonautic/urls.py
@@ -1,10 +1,12 @@
-from django.contrib import admin
 from django.urls import path
+from django.contrib import admin
+from django.conf.urls import url, include
 
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    url(r'^articles/', include('articles.urls')),
     path('about/', views.about),
     path('', views.homepage)
 ]
